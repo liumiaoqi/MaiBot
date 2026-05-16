659b1e09b1c0fa6b9c7fd4409ed9272dd5dc62f2
SengokuCola <1026294844@qq.com>
2026-05-16 18:35:29 +0800

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -58,7 +58,7 @@ BOT_CONFIG_PATH: Path = (CONFIG_DIR / "bot_config.toml").resolve().absolute()
 MODEL_CONFIG_PATH: Path = (CONFIG_DIR / "model_config.toml").resolve().absolute()
 LEGACY_ENV_PATH: Path = (PROJECT_ROOT / ".env").resolve().absolute()
 A_MEMORIX_LEGACY_CONFIG_PATH: Path = (CONFIG_DIR / "a_memorix.toml").resolve().absolute()
-MMC_VERSION: str = "1.0.0-pre.20"
+MMC_VERSION: str = "1.0.0-pre.21"
 CONFIG_VERSION: str = "8.12.9"
 MODEL_CONFIG_VERSION: str = "1.17.0"
 
