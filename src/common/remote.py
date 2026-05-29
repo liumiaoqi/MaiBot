0a86298f3418aa92507490004cfb52bffd8192dc
SengokuCola <1026294844@qq.com>
2026-05-30 00:26:07 +0800

    Update remote.py

--- src/common/remote.py
+++ src/common/remote.py
@@ -181,8 +181,8 @@ class TelemetryHeartBeatTask(AsyncTask):
 
 
 class TelemetryStatsUploadTask(AsyncTask):
-    STATS_UPLOAD_INTERVAL = 3600
-    MAX_LOOKBACK_SECONDS = 24 * 60 * 60
+    STATS_UPLOAD_INTERVAL = 15*60
+    MAX_LOOKBACK_SECONDS = 7 * 24 * 60 * 60
     RETRY_DELAYS = (5, 15, 45)
     CURSOR_KEY = "telemetry_stats_cursor"
 
