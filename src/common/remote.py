f95139da36beddd2aecf7ed0a98c73659df8dbed
SengokuCola <1026294844@qq.com>
2026-06-06 01:38:04 +0800

    放宽遥测上传时间

--- src/common/remote.py
+++ src/common/remote.py
@@ -181,8 +181,8 @@ class TelemetryHeartBeatTask(AsyncTask):
 
 
 class TelemetryStatsUploadTask(AsyncTask):
-    STATS_UPLOAD_INTERVAL = 3637
-    MAX_LOOKBACK_SECONDS = 7 * 24 * 60 * 60
+    STATS_UPLOAD_INTERVAL = 11451
+    MAX_LOOKBACK_SECONDS = 14 * 24 * 60 * 60
     RETRY_DELAYS = (5, 15, 45)
     CURSOR_KEY = "telemetry_stats_cursor"
 
