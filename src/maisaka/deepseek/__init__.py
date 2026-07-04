ccf1010551a811284747cd9016a5604ea086007a
liumiaoqi <68047429+liumiaoqi@users.noreply.github.com>
2026-07-04 10:36:24 +0800

    chore(deepseek): 更新模块导出

--- src/maisaka/deepseek/__init__.py
+++ src/maisaka/deepseek/__init__.py
@@ -1,11 +1,24 @@
 """DeepSeek 深度优化模块。"""
 
+from .batch_scheduler import BatchScheduler, BatchTask, BatchTaskType
 from .budget import TokenBudgetAllocation, TokenBudgetManager
+from .cost_tracker import CostTracker, CostRecord
+from .model_scheduler import ModelScheduler, ModelTier
 from .optimizer import ContextSegment, DeepSeekOptimizer
+from .prefix_cache import PrefixCacheManager, PrefixLayer
 
 __all__ = [
+    "BatchScheduler",
+    "BatchTask",
+    "BatchTaskType",
     "ContextSegment",
+    "CostRecord",
+    "CostTracker",
     "DeepSeekOptimizer",
+    "ModelScheduler",
+    "ModelTier",
+    "PrefixCacheManager",
+    "PrefixLayer",
     "TokenBudgetAllocation",
     "TokenBudgetManager",
 ]
